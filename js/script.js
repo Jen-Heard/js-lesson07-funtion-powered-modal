6bca0ff171d7a50bfde47b01ed5e9ca31d3f3bc3
Jen-Heard <jlheard11@gmail.com>
2024-12-10 14:14:32 -0600

    Deleted and recompleted exercise

--- js/script.js
+++ js/script.js
@@ -2,30 +2,29 @@ var button = document.querySelector(".got-this");
 var modal = document.querySelector(".modal");
 var modalX = document.querySelector(".modal-x");
 
+button.addEventListener("click", function () {
+    openModal();
+});
+
+modalX.addEventListener("click", function () {
+ closeModal();
+});
+
+document.addEventListener("keydown", function(e) {
+  // console.log(e.key);
+  if (e.key === "Escape") {
+    if (modal.classList.contains("show-modal")) {
+     closeModal();
+    }
+  } 
+});
+
 var closeModal = function() {
   modal.classList.remove("show-modal");
   button.innerText = "Who's got this?";
 };
 
-var openModal = function () {
+var openModal = function() {
   modal.classList.add("show-modal");
   button.innerText = "You've got this!!!";
-}
-
-button.addEventListener("click", function () {
-openModal()
-});
-
-modalX.addEventListener("click", function () {
-  closeModal();
-});
-
-document.addEventListener("keydown", function (e) {
-// console.log(e.key);
-if (e.key === "Escape"){
-  // console.log("escape key pressed");
-  if (modal.classList.contains("show-modal")) {
-   closeModal();
-    }
-}
-});
+};
